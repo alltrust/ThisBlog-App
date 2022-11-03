dfc403712ee421399406cd9179b8878b003c60a5
Aldo <aldorgarcia29@gmail.com>
2022-11-03 10:04:05 -0400

    cleanup

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
-
 const adminController = require('../controllers/admin')
 
 router.get('/add-blog', adminController.getAddBlogs)
-
 router.get('/blogs', adminController.getBlogs )
-
 router.post('/add-blog', adminController.postAddBlogs)
-
 router.get('/edit-blog/:blogId', adminController.getEditBlogs)
-
 router.post('/edit-blog', adminController.postEditBlogs)
-
 router.post('/delete-item', adminController.postDeleteBlog)
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,5 @@
 const express = require("express");
-
 const router = express.Router();
-
 const userContoller = require("../controllers/user");
 
 router.get("/", userContoller.getIndex);
@@ -11,7 +9,6 @@ router.get("/blogs/:blogId", userContoller.getBlogDetails);
 
 router.get("/favourites", userContoller.getFavourites);
 router.post("/favourites", userContoller.postFavourites);
-
 router.post("/delete-favourite-item", userContoller.postDeleteFavourite);
 
 router.post("/add-comment", userContoller.postAddComment);
